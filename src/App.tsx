ff610983bc1e0c196bfb16cfae5c33b68d70c7c3
Gabriel-Suldine <gabriel.suldinne@gmail.com>
2023-05-03 22:45:34 -0400

    Inicializando estrutura do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-function App() {
-  return (
-    <>
-    <h1>todoList</h1>
+import { AppBar, Button, Container, Grid, TextField, Toolbar, Typography, useTheme } from "@mui/material"
+import { Theme } from "./Theme"
 
-    </>
+function App() {
+  const theme = useTheme()
+  return (
+    <Theme>
+      <AppBar position="static">
+        <Toolbar sx={{
+          paddingTop: theme.spacing(2),
+          paddingBottom: theme.spacing(2),
+          display: "flex",
+          justifyContent: "center",
+          alignItems: 'center',
+          minHeight: '200px',
+          '@media all': {
+            minHeight: 200,
+          }
+        }}>
+          <Typography variant="h5" component="h1">Todo </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container>
+          <Grid container spacing={theme.spacing(0.5)}>
+            <Grid item xl={10} sm={12}>
+              <TextField name="task" fullWidth />
+            </Grid>
+            <Grid item xl={2} sm={12}>
+              <Button variant="contained" fullWidth>Criar</Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+
+
+
+    </Theme>
+
+
 
   )
 }
